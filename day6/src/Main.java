1aa8e8e3ee90c28012b45be24daa2f0e964cfb25
mauriciopolvora <mauricio.polvora@gmail.com>
2025-12-06 16:11:07 +0000

    day 6 start

---
+++ day6/src/Main.java
@@ -0,0 +1,17 @@
+public static List<String> readFile(String fileName) {
+    // Created List of String
+    List<String> lines = Collections.emptyList();
+
+    try {
+        lines = Files.readAllLines(
+                Paths.get(fileName),
+                StandardCharsets.UTF_8);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+    return lines;
+}
+void main() {
+
+}
