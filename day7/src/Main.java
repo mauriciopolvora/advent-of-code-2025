4d3c4e085d14bd6ea8f300853903bc5db921da69
mauriciopolvora <mauricio.polvora@gmail.com>
2025-12-08 15:24:50 +0000

    day 7

---
+++ day7/src/Main.java
@@ -0,0 +1,26 @@
+public static List<String> readFile(String fileName) {
+    // Created List of String
+    List<String> lines = Collections.emptyList();
+
+    try {
+        lines = Files.readAllLines(
+                Paths.get(fileName),
+                StandardCharsets.UTF_8);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+    return lines;
+}
+
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello and welcome!"));
+
+    for (int i = 1; i <= 5; i++) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        IO.println("i = " + i);
+    }
+}
