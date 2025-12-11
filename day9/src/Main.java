ec89e448da1c308550b3247ac344027ee5eb960a
mauriciopolvora <mauricio.polvora@gmail.com>
2025-12-11 13:47:26 +0000

    day 9 start

---
+++ day9/src/Main.java
@@ -0,0 +1,19 @@
+
+public static List<String> readFile(String fileName) {
+    // Created List of String
+    List<String> lines = Collections.emptyList();
+
+    try {
+        lines = Files.readAllLines(
+                Paths.get(fileName),
+                StandardCharsets.UTF_8);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+    return lines;
+}
+
+void main() {
+    IO.println("Hello World!");
+}
